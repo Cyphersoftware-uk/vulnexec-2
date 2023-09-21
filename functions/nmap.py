2c1446b9b3c322c593255d079262a653737e746b
Jake <59755030+jake0001-dev@users.noreply.github.com>
2023-09-21 07:07:09 +0100

    initial

---
+++ functions/nmap.py
@@ -0,0 +1,17 @@
+import os
+import dotenv
+
+class NmapScan:
+    def __init__():
+        self.
+        pass
+
+    def basic(self):
+        # Write Functions In Here
+        pass
+    
+    def inDepth(self):
+        # Write Function In Here
+        pass
+
+    
